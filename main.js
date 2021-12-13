ed5ba270ec4ea058c37b6a61771b84e35a37aad7
priyan <45042030+priyan1995@users.noreply.github.com>
2021-12-13 19:31:14 +0530

    flower draw

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const canvas =  document.getElementById('canvas1');
+const canvas = document.getElementById('canvas1');
 // console.log(canvas)
 
 const ctx = canvas.getContext('2d');
@@ -7,32 +7,44 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let size = 20;
-let positionX = canvas.width/2;
-let positionY = canvas.height/2;
-let angle = 0;
+// let positionX = canvas.width/2;
+// let positionY = canvas.height/2;
+// let angle = 0;
 
+let number = 0;
+let scale = 10;
 
-function drawFlower(){
+function drawFlower() {
+
+ 
+    let angle = number * 1;
+    let radius = scale * Math.sqrt(number);
+
+    let positionX = radius * Math.sin(angle) + canvas.width/2;
+    let positionY = radius *  Math.cos(angle) + canvas.height/2;
+
     ctx.fillStyle = 'red';
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = 5;
     ctx.beginPath();
-    ctx.arc(positionX,positionY,20,0,Math.PI*2);
+    ctx.arc(positionX, positionY, 20, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
+
+    number++;
 }
 
 
-function animate(){
+function animate() {
 
     //ctx.clearRect(0,0,canvas.width, canvas.height);
 
     // size +=0.3;
-    positionX += 5 * Math.sin(angle);    
-    positionY += 5 * Math.cos(angle);
-    angle += 0.1;
-    drawFlower();  
+    // positionX += 5 * Math.sin(angle);
+    // positionY += 5 * Math.cos(angle);
+    // angle += 0.1;
+    drawFlower();
     requestAnimationFrame(animate);
 }
 
