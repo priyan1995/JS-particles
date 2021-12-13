f8bcf1b21a12e9a50beebf277a1b62a1ba4b80d1
priyan <45042030+priyan1995@users.noreply.github.com>
2021-12-13 19:18:45 +0530

    circular movement

--- main.js
+++ main.js
@@ -6,12 +6,34 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.fillStyle = 'red';
-ctx.strokeStyle = 'blue';
-ctx.lineWidth = 5;
-ctx.beginPath();
-ctx.arc(100,100,50,0,Math.PI*2);
-ctx.closePath();
-ctx.fill();
+let size = 20;
+let positionX = canvas.width/2;
+let positionY = canvas.height/2;
+let angle = 0;
 
-ctx.stroke();
+
+function drawFlower(){
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.arc(positionX,positionY,20,0,Math.PI*2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+}
+
+
+function animate(){
+
+    //ctx.clearRect(0,0,canvas.width, canvas.height);
+
+    // size +=0.3;
+    positionX += 5 * Math.sin(angle);    
+    positionY += 5 * Math.cos(angle);
+    angle += 0.1;
+    drawFlower();  
+    requestAnimationFrame(animate);
+}
+
+animate();
